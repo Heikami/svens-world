3dddc316cad782cfbbfbdc8bf6ace65476250c85
Tyler <tylerkhuestis2@gmail.com>
2025-05-25 19:04:47 -0400

    Initial commit

---
+++ src/main/java/ca/svens/world/SvensWorldClient.java
@@ -0,0 +1,11 @@
+package ca.svens.world;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public  class SvensWorldClient implements ClientModInitializer {
+        @Override
+    public void onInitializeClient() {
+
+        }
+}
+
